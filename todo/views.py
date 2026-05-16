0254afdda69cf50e257848bd04fd501e6744199b
rislamahad <rislamahad@gmail.com>
2026-05-16 12:43:01 +0600

    first output

--- todo/views.py
+++ todo/views.py
@@ -1,3 +1,18 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.http import HttpRequest
+from .models import Todo
 
 # Create your views here.
+def list_todo_items(request):
+    context = {'todo_list': Todo.objects.all()}
+    return render(request, 'todo/todo_list.html', context)
+
+def insert_todo_item(request:HttpRequest):
+    todo = Todo(content = request.POST['content'])
+    todo.save()
+    return redirect('/todo/list/')
+
+def delete_todo_item(request, todo_id):
+    todo_delete = Todo.objects.get(id= todo_id)
+    todo_delete.delete()
+    return redirect('/todo/list/')
--- todo_app/settings.py
+++ todo_app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'todo'
 ]
 
 MIDDLEWARE = [
@@ -74,8 +75,11 @@ WSGI_APPLICATION = 'todo_app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'TodoDB',
+        'USER': 'postgres',
+        'PASSWORD': 'postgres',
+        'HOST': 'localhost'
     }
 }
 
